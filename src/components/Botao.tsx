17b31379d7544d52acc7a6ceee818332b7f9df1d
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-07-02 23:39:50 -0300

    ajuste no codigo

--- src/components/Botao.tsx
+++ src/components/Botao.tsx
@@ -12,8 +12,7 @@ export default function Botao(props: BotaoProps){
          text-white px-4 py-2 
          rounded-md
          ${props.className}
-         `
-         }>
+         `}>
             {props.children}
         </button>
     );
